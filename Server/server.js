9aff70c33aa0121ca5466292157e97deb1e476e2
slashleo <37038834+slashleo@users.noreply.github.com>
2021-07-16 10:34:45 +0200

    Fix path in server.js

--- Server/server.js
+++ Server/server.js
@@ -5,10 +5,10 @@ const path = require("path");
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, "../client/build")));
+app.use(express.static(path.join(__dirname, "../Client/build")));
 //app.get("/*", (req, res) => res.sendFile(path.join(__dirname, "./index.html")));
 app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+  res.sendFile(path.join(__dirname, "../Client/build", "index.html"));
 });
 
 const PORT = process.env.PORT || 3000;
